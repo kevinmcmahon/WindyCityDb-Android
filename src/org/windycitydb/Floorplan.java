219d952d02ce2dd90f83ae3f65bf174cf0410ae9
Kevin McMahon <klmcmahon+github@gmail.com>
2011-06-24 19:50:10 -0500

    added alternate textview display when floorplan image not available

--- src/org/windycitydb/Floorplan.java
+++ src/org/windycitydb/Floorplan.java
@@ -4,9 +4,13 @@ import org.windycitydb.util.Network;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Floorplan extends Activity {
 	private static final String CLASSTAG = Floorplan.class.getSimpleName();
@@ -18,12 +22,19 @@ public class Floorplan extends Activity {
         
         setContentView(R.layout.floorplan);
         
+        TextView textView = (TextView) findViewById(R.id.floorplan_text);
         ImageView imageView = (ImageView) findViewById(R.id.floorplan_image);
         String url = getIntent().getStringExtra(Constants.FLOOR_PLAN_URL_EXTRA);
         
         if(url != null && url != "") {
 	        Bitmap bitmap = Network.downloadBitmap(url);
 	        imageView.setImageBitmap(bitmap);
+	        imageView.setVisibility(View.VISIBLE);
+	        textView.setVisibility(View.INVISIBLE);
+        } else {
+        	imageView.setVisibility(View.INVISIBLE);
+        	textView.setVisibility(View.VISIBLE);
+        	textView.setGravity(Gravity.CENTER);
         }
 	}
 }
